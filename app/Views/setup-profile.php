2a002907b50690a14a5286c43e662229970f56df
migambile25 <migambile25@gmail.com>
2024-05-27 11:32:18 +0300

    Setup Profile Page

--- app/Views/setup-profile.php
+++ app/Views/setup-profile.php
@@ -81,7 +81,7 @@ $user_level = $user["user_level"];
     </div>
     <!-- Form Parts -->
     <div class="mt-10 border-t border-slate-200/60 px-5 pt-10 dark:border-darkmode-400 sm:px-20">
-        <form name="profile-setup" class="" action="/" method="post">
+        <form name="profile-setup" class="" action="/profile-setup" method="post">
             <!-- Step 1 Form Part -->
             <div class="form-part" id="form-part-1">
                 <div class="text-base font-medium">Your Personal Profile</div>
@@ -116,7 +116,7 @@ $user_level = $user["user_level"];
                         <input data-tw-merge="" required name="user_surname"
                                id="input-wizard-1" type="text"
                                placeholder="Your Surname"
-                               value="<?= $user['user_lastname'] ?? "" ?>"
+                               value="<?= $user['user_surname'] ?? "" ?>"
                                class="form-input transition duration-200 ease-in-out w-full text-sm border-slate-200 shadow-sm rounded-md placeholder:text-slate-400/90 focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus:border-primary focus:border-opacity-40 dark:bg-darkmode-800 dark:border-transparent dark:focus:ring-slate-700 dark:focus:ring-opacity-50 dark:placeholder:text-slate-500/80 group-[.form-inline]:flex-1 group-[.input-group]:rounded-none group-[.input-group]:[&:not(:first-child)]:border-l-transparent group-[.input-group]:first:rounded-l group-[.input-group]:last:rounded-r group-[.input-group]:z-10">
                     </div>
                     <div class="intro-y col-span-12 sm:col-span-6">
